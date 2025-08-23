bce9bfc5978dd5a7456b497332d7e31ed5cc109f
basit-dev <basitdev@icloud.com>
2025-08-23 16:22:48 +0100

    Add user, base template
    Add suer page and base template added

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def welcome():
     return render_template("welcome.html")
 
 @app.route("/users")
 def users():
     return render_template("users.html")
+
+@app.route("/add")
+def add_user():
+    return render_template("add_user.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
